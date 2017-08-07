ec2599e04f7f16df0dfc0ac02d00f4e793947a5d
Shan <slu5@wellesley.edu>
2017-08-07 11:55:58 -0700

    add tootgleInlineStyle

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 
 /* This can check if your electron app can communicate with your backend */
@@ -15,22 +15,58 @@ class MyEditor extends React.Component {
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
   }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      "BOLD"
+    ));
+  }
+
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      "ITALIC"
+    ));
+  }
+
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      "ITALIC"
+    ));
+  }
+
+  _onUnderlineClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      "UNDERLINE"
+    ));
+  }
+
+  _onStrikeClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'STRIKETHROUGH'
+    ));
+  }
+
   render() {
     return (
-      <div>
+      <div style={{ margin: "20px" }}>
         <p className="docHeader">Edit your docs:</p>
         <p>Document ID: testID</p>
         <button type="button" className="saveButton">Save Changes</button>
         <div className="toolbar">
-          <button className="styleButton" type="button"> <span className="glyphicon glyphicon-bold"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-italic"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-text-color"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-align-left"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-align-center"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-align-right"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-list"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-text-color"></span></button>
-          <button className="styleButton" type="button"><span className="glyphicon glyphicon-tint"></span></button>
+          <button className="styleButton" type="button" onClick={this._onBoldClick.bind(this)}> <span className="glyphicon glyphicon-bold"></span></button>
+          <button className="styleButton" type="button" onClick={this._onItalicClick.bind(this)}><span className="glyphicon glyphicon-italic"></span></button>
+          <button className="styleButton" type="button" onClick={this._onUnderlineClick.bind(this)}><span className="glyphicon glyphicon-text-color"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-left"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-center"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-right"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-list"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-text-color"></span></button>
+          <button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-tint"></span></button>
         </div>
         <div className="editor">
           <Editor editorState={this.state.editorState} onChange={this.onChange} />
