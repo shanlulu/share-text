222c10395f8c229a36e91b8270e05699bc856c5e
blerude <benjamin.lerude@yale.edu>
2017-08-07 14:35:14 -0700

    Block styling

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {Editor, EditorState, RichUtils, DefaultDraftBlockRenderMap} from 'draft-js';
+import Immutable from 'immutable'
 
 
 /* This can check if your electron app can communicate with your backend */
@@ -8,6 +9,41 @@ import {Editor, EditorState, RichUtils} from 'draft-js';
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
+
+const styleMap = {
+  'SIZE_10': {
+    fontSize: 10
+  },
+  'SIZE_12': {
+    fontSize: 12
+  },
+  'SIZE_16': {
+    fontSize: 16
+  },
+  'SIZE_20': {
+    fontSize: 20
+  },
+  'SIZE_24': {
+    fontSize: 24
+  },
+  'RED': {
+    color: 'red'
+  }
+}
+
+const blockRenderMap = Immutable.Map({
+  'ALIGN_LEFT': {
+    wrapper: <div className='left' />
+  },
+  'ALIGN_CENTER': {
+    wrapper: <div className='center' />
+  },
+  'ALIGN_RIGHT': {
+    wrapper: <div className='right' />
+  }
+})
+
+const extendedBlockRenderMap = DefaultDraftBlockRenderMap.merge(blockRenderMap);
 
 class MyEditor extends React.Component {
   constructor(props) {
@@ -17,13 +53,15 @@ class MyEditor extends React.Component {
     this.toggleBlockType = (type) => this._toggleBlockType(type);
   }
 
-  // _onStrikeClick() {
-  //   this.onChange(RichUtils.toggleInlineStyle(
-  //     this.state.editorState,
-  //     'STRIKETHROUGH'
-  //   ));
-  // }
-  //
+  _onFontSizeClick() {
+    var newSize = document.getElementById('textSizePicker').value;
+    console.log(newSize)
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'SIZE_16'
+    ));
+  }
+
   _onStrikeClick() {
     this.onChange(RichUtils.toggleInlineStyle(
       this.state.editorState,
@@ -38,6 +76,13 @@ class MyEditor extends React.Component {
     ));
   }
 
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      "ITALIC"
+    ));
+  }
+
   _onUnderlineClick() {
     this.onChange(RichUtils.toggleInlineStyle(
       this.state.editorState,
@@ -48,21 +93,21 @@ class MyEditor extends React.Component {
   _onLeftAlignClick() {
     this.onChange(RichUtils.toggleBlockType(
       this.state.editorState,
-      "ALIGN-LEFT"
+      "ALIGN_LEFT"
     ));
   }
 
   _onCenterAlignClick() {
     this.onChange(RichUtils.toggleBlockType(
       this.state.editorState,
-      "ALIGN-CENTER"
+      "ALIGN_CENTER"
     ));
   }
 
   _onRightAlignClick() {
     this.onChange(RichUtils.toggleBlockType(
       this.state.editorState,
-      "ALIGN-RIGHT"
+      "ALIGN_RIGHT"
     ));
   }
 
@@ -80,6 +125,10 @@ class MyEditor extends React.Component {
     ));
   }
 
+  blockRenderer(block) {
+    console.log('type: ', block.getType())
+  }
+
 
   render() {
     return (
@@ -88,30 +137,37 @@ class MyEditor extends React.Component {
         <p>Document ID: testID</p>
         <button type="button" className="saveButton">Save Changes</button>
         <div className="toolbar">
-          <span title="Change Text Size"><button className="styleButton glyphicon glyphicon-text-size" type="button"> <select>
-            <option value="10">10</option>
-            <option value="12">12</option>
-            <option value="14">14</option>
-            <option value="16">16</option>
-            <option value="18">18</option>
-            <option value="20">20</option>
-            <option value="24">24</option>
-          </select></button></span>
+          <span title="Change Text Size">
+            <button
+              className="styleButton glyphicon glyphicon-text-size"
+              type="button"
+              onChange={this._onFontSizeClick.bind(this)}>
+              <select className="textSizePicker">
+                <option value="10">10</option>
+                <option value="12">12</option>
+                <option value="14">16</option>
+                <option value="20">20</option>
+                <option value="24">24</option>
+              </select>
+            </button>
+          </span>
           <span title="Change Text Color"><button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-tint"></span></button></span>
           <span title="Bold"><button className="styleButton" type="button" onClick={this._onBoldClick.bind(this)}><span className="glyphicon glyphicon-bold"></span></button></span>
           <span title="Italicize"><button className="styleButton" type="button" onClick={this._onItalicClick.bind(this)}><span className="glyphicon glyphicon-italic"></span></button></span>
           <span title="Underline"><button className="styleButton" type="button" onClick={this._onUnderlineClick.bind(this)}><span className="glyphicon glyphicon-text-color"></span></button></span>
-          <span title="Align Left"><button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-left"></span></button></span>
-          <span title="Align Center"><button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-center"></span></button></span>
-          <span title="Align Right"><button className="styleButton" type="button" onClick={this._onStrikeClick.bind(this)}><span className="glyphicon glyphicon-align-right"></span></button></span>
+          <span title="Align Left"><button className="styleButton" type="button" onClick={this._onLeftAlignClick.bind(this)}><span className="glyphicon glyphicon-align-left"></span></button></span>
+          <span title="Align Center"><button className="styleButton" type="button" onClick={this._onCenterAlignClick.bind(this)}><span className="glyphicon glyphicon-align-center"></span></button></span>
+          <span title="Align Right"><button className="styleButton" type="button" onClick={this._onRightAlignClick.bind(this)}><span className="glyphicon glyphicon-align-right"></span></button></span>
           <span title="Bullet List"><button className="styleButton" type="button" onClick={this._onULClick.bind(this)}><span className="glyphicon glyphicon-list"></span></button></span>
           <span title="Numbered List"><button className="styleButton" type="button" onClick={this._onOLClick.bind(this)}><span className="glyphicon glyphicon-sort-by-order"></span></button></span>
         </div>
         <div className="editor">
           <Editor
+            customStyleMap={styleMap}
             editorState={this.state.editorState}
             onChange={this.onChange}
             placeholder="Enter your text below"
+            blockRenderMap={extendedBlockRenderMap}
           />
         </div>
       </div>
